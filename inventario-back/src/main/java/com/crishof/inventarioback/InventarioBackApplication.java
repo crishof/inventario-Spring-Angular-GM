61bcee8d5b335ae9479751355a3c2f44459376a2
crishof <cristianhof@gmail.com>
2024-03-07 21:56:24 +0100

    first commit

---
+++ inventario-back/src/main/java/com/crishof/inventarioback/InventarioBackApplication.java
@@ -0,0 +1,13 @@
+package com.crishof.inventarioback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioBackApplication.class, args);
+	}
+
+}
---
+++ inventario-back/src/test/java/com/crishof/inventarioback/InventarioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crishof.inventarioback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
